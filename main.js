9b8e48c4e13412c177c1a612c8f211f2933c23de
elasri21 <islame.edu@gmail.com>
2023-12-08 10:06:45 +0100

    ts

--- main.js
+++ main.js
@@ -2,15 +2,14 @@
 let questions = Array.from(document.querySelectorAll("div.qu"));
 let answers = Array.from(document.querySelectorAll("div.an"));
 
-questions.forEach(qu => {
-    qu.addEventListener("click", function() {
-        for (let i = 0; i < questions.length; i++)
-        {
-            questions[i].classList.remove("clicked");
-        }
-        qu.nextElementSibling
-        answers.forEach(an => an.classList.remove("shown"));
-        this.classList.add("clicked");
-        this.nextElementSibling.classList.add("shown");
-    });
+questions.forEach((qu) => {
+  qu.addEventListener("click", function () {
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].classList.remove("clicked");
+    }
+    qu.nextElementSibling;
+    answers.forEach((an) => an.classList.remove("shown"));
+    this.classList.add("clicked");
+    this.nextElementSibling.classList.add("shown");
+  });
 });
